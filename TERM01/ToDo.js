bd2b0a5eb4ecfd29f82d65f6870fde9648c1f1bd
seunghyuk <sunghyuk9@naver.com>
2018-05-01 03:01:55 +0900

    Remove method impleted by Array & openModifyModal

--- TERM01/ToDo.js
+++ TERM01/ToDo.js
@@ -1,5 +1,5 @@
-var modal;
-var formItemInModal; //  0:select DAY  1:Name  2:Contents
+var addModal; var modifyModal;
+var formItemInAddModal; //  0:select DAY  1:Name  2:Contents
 var monItem=[]; var monIndex=0;
 var tueItem=[]; var tueIndex=0;
 var wedItem=[]; var wedIndex=0;
@@ -9,19 +9,19 @@ var friItem=[]; var friIndex=0;
 //var divItem;
 
 // AddToDo 클릭시 modalopen
-function openModal() {
-    modal= document.getElementById("MyModal");
-    modal.style.display = 'block';
+function openAddModal() {
+    addModal= document.getElementById("addModal");
+    addModal.style.display = 'block';
 }
 
 // x표 누를시 modalclose
-function closeModal() {
-    modal.style.display = 'none';
+function closeAddModal() {
+    addModal.style.display = 'none';
 }
 
 function addItem(){
-  formItemInModal=document.getElementsByClassName('formInner_inModal');
-  switch (formItemInModal[0].value) {
+  formItemInAddModal=document.getElementsByClassName('formInner_inAddModal');
+  switch (formItemInAddModal[0].value) {
     case "monday":
       var areaMon = document.getElementsByClassName('displayArea_mon')[0];
       this.monItem[this.monIndex]=makeDiv(areaMon); // 배열로관리
@@ -55,126 +55,57 @@ function addItem(){
     default:
       break;
   }
-closeModal();
+closeAddModal();
   //formItemInModal[0].
 }
 
 /***********makeDiv**********/
 function makeDiv(area){
+  var itemName = this.formItemInAddModal[1].value;
+  var divItem = document.createElement('div');
+  divItem.setAttribute("class","divItem");
+  divItem.setAttribute("onclick","openModifyModal(this)");
+  var close = document.createElement('img');
+  close.setAttribute("class", "close");//id부여
+  close.setAttribute("src","img/delete.png");
+  close.setAttribute("onclick","removeItem(this,event)");
+  divItem.appendChild(close);
+  var textDiv = document.createElement('div');
+  textDiv.setAttribute("class", "itemName");//id부여
+  textDiv.innerHTML = itemName;
+  divItem.appendChild(textDiv);
+  area.appendChild(divItem);
 
   switch (area.className) {
     case "displayArea_mon":
-      var itemName = this.formItemInModal[1].value;
-      var divItem = document.createElement('div');
-      divItem.setAttribute("id","divItem");
       divItem.setAttribute("index",monIndex);
       divItem.style.backgroundColor = "#cfe0e0";
-      //console.log(monIndex); //start expected 0
-      //div.setAttribute('index',this.monIndex);
-      var close = document.createElement('span');
-      close.setAttribute("class", "close");//id부여
-      close.setAttribute("onclick","removeItem(this)");
-      //  close.setAttribute("font-size"," 12px");
-      close.innerHTML='&times;';
-      divItem.appendChild(close);
-      var textDiv = document.createElement('div');
-      textDiv.setAttribute("class", "itemName");//id부여
-      textDiv.innerHTML = itemName;
-      divItem.appendChild(textDiv);
-      area.appendChild(divItem);
-
-      return divItem;
       break;
-
 
     case "displayArea_tue":
-      var itemName = this.formItemInModal[1].value;
-      var divItem = document.createElement('div');
-      divItem.setAttribute("id","divItem");
       divItem.setAttribute("index",tueIndex);
       divItem.style.backgroundColor = "#e9b7b7";
-      var close = document.createElement('span');
-      close.setAttribute("class", "close");
-      close.setAttribute("onclick","removeItem(this)");
-      close.innerHTML='&times;';
-      divItem.appendChild(close);
-      var textDiv = document.createElement('div');
-      textDiv.setAttribute("class", "itemName");
-      textDiv.innerHTML = itemName;
-      divItem.appendChild(textDiv);
-      area.appendChild(divItem);
-
-      return divItem;
       break;
-
-
 
     case "displayArea_wed":
-
-      var itemName = this.formItemInModal[1].value;
-      var divItem = document.createElement('div');
-      divItem.setAttribute("id","divItem");
       divItem.setAttribute("index",wedIndex);
       divItem.style.backgroundColor = "#cce0e0";
-      var close = document.createElement('span');
-      close.setAttribute("class", "close");
-      close.setAttribute("onclick","removeItem(this)");
-      close.innerHTML='&times;';
-      divItem.appendChild(close);
-      var textDiv = document.createElement('div');
-      textDiv.setAttribute("class", "itemName");
-      textDiv.innerHTML = itemName;
-      divItem.appendChild(textDiv);
-      area.appendChild(divItem);
-
-      return divItem;
       break;
-
 
     case "displayArea_thu":
-      var itemName = this.formItemInModal[1].value;
-      var divItem = document.createElement('div');
-      divItem.setAttribute("id","divItem");
       divItem.setAttribute("index",thuIndex);
       divItem.style.backgroundColor = "#e2ddc0";
-      var close = document.createElement('span');
-      close.setAttribute("class", "close");
-      close.setAttribute("onclick","removeItem(this)");
-      close.innerHTML='&times;';
-      divItem.appendChild(close);
-      var textDiv = document.createElement('div');
-      textDiv.setAttribute("class", "itemName");
-      textDiv.innerHTML = itemName;
-      divItem.appendChild(textDiv);
-      area.appendChild(divItem);
-
-      return divItem;
       break;
-
 
     case "displayArea_fri":
-      var itemName = this.formItemInModal[1].value;
-      var divItem = document.createElement('div');
-      divItem.setAttribute("id","divItem");
       divItem.setAttribute("index",friIndex);
       divItem.style.backgroundColor = "skyblue";
-      var close = document.createElement('span');
-      close.setAttribute("class", "close");
-      close.setAttribute("onclick","removeItem(this)");
-      close.innerHTML='&times;';
-      divItem.appendChild(close);
-      var textDiv = document.createElement('div');
-      textDiv.setAttribute("class", "itemName");
-      textDiv.innerHTML = itemName;
-      divItem.appendChild(textDiv);
-      area.appendChild(divItem);
-
-      return divItem;
       break;
+
     default:
       break;
   }
-
+  return divItem;
 }
 /*
 window.onclick = function(event) {
@@ -196,27 +127,20 @@ function showItem(area){
 }
 */
 //***********deleteDiv***********//
-function removeItem(obj){
-  console.log("removeItem 메소드 실행");
-  console.log(obj.parentNode);
-  console.log(obj.parentNode.parentNode);
-
-  console.log(this.monItem);
-  console.log(this.monIndex);
-//  obj.parentNode.getAttribute('index');
-
-  //monIndex--;
+function removeItem(obj,event){
+  event.stopPropagation();
   switch (obj.parentNode.parentNode.className) {
     case "displayArea_mon":
       var target = obj.parentNode.getAttribute('index');
+      this.monItem[target].remove();
       this.monItem.splice(target,1);
       this.monIndex--;
-      // sort 해주기
       indexSort(this.monItem);
       break;
 
     case "displayArea_tue":
       var target = obj.parentNode.getAttribute('index');
+      this.tueItem[target].remove();
       this.tueItem.splice(target,1);
       this.tueIndex--;
       indexSort(this.tueItem);
@@ -225,6 +149,7 @@ function removeItem(obj){
 
     case "displayArea_wed":
       var target = obj.parentNode.getAttribute('index');
+      this.wedItem[target].remove();
       this.wedItem.splice(target,1);
       this.wedIndex--;
       indexSort(this.wedItem);
@@ -233,6 +158,7 @@ function removeItem(obj){
 
     case "displayArea_thu":
       var target = obj.parentNode.getAttribute('index');
+      this.thuItem[target].remove();
       this.thuItem.splice(target,1);
       this.thuIndex--;
       indexSort(this.thuItem);
@@ -241,19 +167,38 @@ function removeItem(obj){
 
     case "displayArea_fri":
       var target = obj.parentNode.getAttribute('index');
+      this.friItem[target].remove();
       this.friItem.splice(target,1);
       this.friIndex--;
       indexSort(this.friItem);
 
       break;
     default:
-    break;
+      break;
   }
-  obj.parentNode.parentNode.removeChild(obj.parentNode);
+  console.log("removeItem 메소드 실행");
+  console.log(obj.parentNode);
+  //console.log(obj.parentNode.parentNode);
+  console.log(monItem);
+  console.log(monIndex);
+  //obj.parentNode.parentNode.removeChild(obj.parentNode);
 }
 
-function indexSort(array){ // monItem 배열에 생성된 div의 index를 0부터 재배열
+function indexSort(array){ // monItem 배열에 생성된 div의 index속성을 0부터 재배열
   for (var i = 0; i < array.length; i++) {
     array[i].setAttribute('index',i);
   }
+}
+
+
+
+// AddToDo 클릭시 modalopen
+function openModifyModal() {
+    modifyModal= document.getElementById("modifyModal");
+    modifyModal.style.display = 'block';
+}
+
+// x표 누를시 modalclose
+function closeModifyModal() {
+    modifyModal.style.display = 'none';
 }
